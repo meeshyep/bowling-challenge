7e784bdb8a547ed7c1253c24e79f0615ea08d977
Michelle Epstein <epstein.michelle@gmail.com>
2016-11-06 21:01:08 +0000

    Added Final Frame, Strike Frame and Spare Frame

--- spec/FrameSpec.js
+++ spec/FrameSpec.js
@@ -2,9 +2,6 @@ describe ("Frame", function() {
 
   beforeEach function() {
     frame = new Frame();
-    finalFrame = new Frame('final');
-    bonusSpareFrame = new Frame('spare');
-    bonusStrikeFrame = new Frame('strike');
   });
 
   describe('Adding scores', function() {
@@ -24,7 +21,7 @@ describe ("Frame", function() {
     it("Knows the result of the frame - normal", function() {
       frame.addScore(5);
       frame.addScore(1);
-      expect(frame.frameResult()).toEqual(null);
+      expect(frame.frameResult()).toEqual('frame');
     });
 
     it("Knows the result of the frame - spare", function() {
@@ -34,7 +31,6 @@ describe ("Frame", function() {
     });
     it("Knows the result of the frame - strike", function() {
       frame.addScore(10);
-      frame.addScore(0);
       expect(frame.frameResult()).toEqual('strike');
     });
   });
@@ -46,73 +42,21 @@ describe ("Frame", function() {
       expect(frame.calculatedScore()).toEqual(0);
     });
 
-    it("Should return sum of pins on a Normal Frame", function() {
+    it("Should return sum of pins", function() {
       frame.addScore(1);
       frame.addScore(2);
       expect(frame.calculatedScore()).toEqual(3);
     })
   });
 
-  describe("Spare Bonus Game", function() {
-    beforeEach(function() {
-      bonusSpareFrame = new Frame('spare');
-    });
-    it "Should add the Bonus of the first Score", function() {  }
-    bonusSpareFrame.addScore(5);
-    bonusSpareFrame.addScore(5);
-    expect(bonusSpareFrame.calculatedScore()).toEqual(10);
-  })
-});
-
-describe("Spare Bonus Game", function(){
-  beforeEach(function() {
-    bonusStrikeFrame = new Frame('strike');
-  });
-  describe("Strike Bonus Game", function(){
-    it "Should add the Bonus of the combined Score", function() {
-      bonusStrikeFrame.addScore(2);
-      bonusStrikeFrame.addScore(3);
-      expect(bonusStrikeFrame.calculatedScore()).toEqual(10);
-    })
-  });
-  describe("Final Frame", function(){
-    it('should add bonus of combined score', function(){
-      finalFrame.addScore(3);
-      finalFrame.addScore(4);
-      expect(finalFrame.calculateScore()).toEqual(7);
-    });
-    it('should add one roll bonus for spare', function(){
-      finalFrame.addScore(3);
-      finalFrame.addScore(7);
-      finalFrame.addScore(5);
-      expect(finalFrame.calculateScore()).toEqual(15);
-    });
-    it('should add one roll bonus for strike', function(){
-      finalFrame.addScore(10);
-      finalFrame.addScore(10);
-      expect(finalFrame.calculateScore()).toEqual(20);
-    });
-    it('should add two roll bonus for doublestrike', function(){
-      finalFrame.addScore(10);
-      finalFrame.addScore(10);
-      finalFrame.addScore(10);
-      expect(finalFrame.calculateScore()).toEqual(30);
-    })
-  });
-
   describe('Defending Against Edge Cases', function() {
-    it("should not accept more than two scores unless final", function() {
+    it("should not accept more than two scores", function() {
       frame.addScore(5);
       frame.addScore(1);
-      expect(function(){frame.addScore(5)}).toThrow(new Error("The Frame in already over"));
+      expect(function(){frame.addScore(5)}).toThrow(new Error("The Frame is already over"));
     });
     it("should not accept two scores over 10", function() {
       frame.addScore(6);
       expect(function(){frame.addScore(7)}).toThrow(new Error("Maximum Frame Score Reached"));
-    });
-    it("should not accept three scores over 30 on final w/ strikes", function() {
-      finalFrame.addScore(10);
-      finalFrame.addScore(10);
-      expect(function(){finalFrame.addScore(15)}).toThrow(new Error("Maximum Frame Score Reached"));
     });
   });
